668c58e0485ea802c504d1facfa7aca99ca9b0dd
shubham-gupta-code <shubham.it7062@gmail.com>
2025-08-27 15:22:36 +0530

    expense package upload

---
+++ 01 - Setup/expense/routes.py
@@ -0,0 +1,15 @@
+from expense import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register')
+def registration_page():
+    return render_template('registration.html')
